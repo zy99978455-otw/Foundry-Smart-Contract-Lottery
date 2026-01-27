7c011fdbe11c2231ca092849be8e215cb75f010c
Zhao Yi <17701139970@163.com>
2026-01-27 21:06:42 +0800

    feat: Integrate Chainlink VRF v2.5 and implement request logic

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,38 +1,53 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.19;
 
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
 /**
  * @title A simple Raffle contract
  * @author ZhaoYi
  * @notice This contract implements a simple raffle system.
  * @dev Implements Chainlink VRF v2.5
  */
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2Plus {
     /* Errors */
     error Raffle__SendMoreToEnterRaffle();
 
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
     uint256 private immutable i_entranceFee;
-    // @dev: The duration of the lottery in seconds
     uint256 private immutable i_interval;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
 
     /* Events */
     event RaffleEntered(address indexed player);
 
-
-    constructor(uint256 entranceFee, uint256 interval) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLine,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp;
+        i_keyHash = gasLine;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
-    function enterRaffle() external payable{
-
+    function enterRaffle() external payable {
         // require(msg.value >= i_entranceFee, "Not enough ETH to enter raffle");
         // require(msg.value >= i_entranceFee, SendMoreToEnterRaffle);
         if (msg.value < i_entranceFee) {
-            revert Raffle__SendMoreToEnterRaffle();     // gas 效率最高
+            revert Raffle__SendMoreToEnterRaffle(); // gas 效率最高
         }
         s_players.push(payable(msg.sender));
 
@@ -47,10 +62,30 @@ contract Raffle {
         if ((block.timestamp - s_lastTimeStamp) < i_interval) {
             revert();
         }
+
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest({
+            keyHash: i_keyHash, //用哪一组随机节点
+            subId: i_subscriptionId,    //谁付钱？
+            requestConfirmations: REQUEST_CONFIRMATIONS,    //你能等多久？
+            callbackGasLimit: i_callbackGasLimit,   //我最多允许你用多少 gas 调我
+            numWords: NUM_WORDS,    //你要几个随机数
+            extraArgs: VRFV2PlusClient._argsToBytes(    //可扩展协议位
+                // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+            )
+        });
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
     }
 
-    /** Getter Functions */
-    function getEntranceFee() external view returns(uint256) {
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {
+        // 这里是以后写“选出赢家”逻辑的地方
+        // 现在留空没关系，只要有这个函数壳子，就能通过编译
+    }
+
+    /**
+     * Getter Functions
+     */
+    function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
 }
